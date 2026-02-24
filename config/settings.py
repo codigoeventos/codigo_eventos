febafe56cfb09024aab8fb563efa08386c6ad7c7
Rafael Pinheiro <rafaelpinheiro2273@gmail.com>
2026-02-24 08:38:48 -0300

    fix: add CSRF_TRUSTED_ORIGINS from env

--- config/settings.py
+++ config/settings.py
@@ -16,6 +16,8 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(',')
+
 # Application definition
 
 INSTALLED_APPS = [
