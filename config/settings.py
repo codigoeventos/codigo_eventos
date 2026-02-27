1bcfd4ebbcc2b39eaec8ec5e0615b7e53eb2ab95
Rafael Pinheiro <rafaelpinheiro2273@gmail.com>
2026-02-27 13:28:51 -0300

    feat: update logic

--- config/settings.py
+++ config/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
 
 # Whitenoise configuration for production static files
 STORAGES = {
